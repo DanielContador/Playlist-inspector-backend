43732b31dcf63ac3a088483784fe3d28deb86c33
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-11-26 00:14:52 -0300

    aa

--- scraper.js
+++ scraper.js
@@ -1,7 +1,16 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeSpotifyPlaylists(artistId) {
-  const browser = await puppeteer.launch({ headless: true }); // Navegador oculto
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: chromium.path, // Usa el path del Chromium instalado
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu'
+    ]
+  });// Navegador oculto
   const page = await browser.newPage();
 // Establecer un userAgent personalizado
 await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
