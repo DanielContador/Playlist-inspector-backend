b7e007496386abc5a29d3e2db10ac43b404c62d4
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-11-26 00:00:11 -0300

    ...

--- scraper.js
+++ scraper.js
@@ -3,6 +3,8 @@ const puppeteer = require('puppeteer');
 async function scrapeSpotifyPlaylists(artistId) {
   const browser = await puppeteer.launch({ headless: true }); // Navegador oculto
   const page = await browser.newPage();
+// Establecer un userAgent personalizado
+await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
 
   const baseUrl = `https://open.spotify.com/intl-es/artist/${artistId}`;
   const allPlaylists = [];
