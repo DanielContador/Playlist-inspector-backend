00ab484a1df88d9971b3b87a0b8b20a5aaedb361
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-11-25 23:30:57 -0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const scrapeSpotifyPlaylists = require('./scraper.js');
-
+const cors = require('cors'); 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
