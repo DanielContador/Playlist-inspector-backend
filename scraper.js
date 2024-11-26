503af9b55f562a8603e8518a0af22d76d97e5c3a
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-11-26 02:08:33 -0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,14 +1,20 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeSpotifyPlaylists(artistId) {
-  const browser = await puppeteer.launch({ headless: true }); // Navegador oculto
+  const browser = await puppeteer.launch({
+    headless: true, // o false para pruebas locales
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security'],
+  }); // Navegador oculto
   const page = await browser.newPage();
-
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+  );
   const baseUrl = `https://open.spotify.com/intl-es/artist/${artistId}`;
   const allPlaylists = [];
 
   try {
-    await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+    await page.goto(baseUrl, { waitUntil: 'networkidle2' });
+   
 
     const pageContent = await page.content();
     console.log('Page content loaded:', pageContent);
