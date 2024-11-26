f9d60a689fe20a0b26c5fb106b0e9c69a36dc9f8
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-11-25 23:04:08 -0300

    ruta scraper

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const scrapeSpotifyPlaylists = require('scraper.js');
+const scrapeSpotifyPlaylists = require('./scraper.js');
 
 const app = express();
 app.use(bodyParser.json());
